6eb6877cc51bac7671954c314d027c19905ab608
yogesh harsana <e20cse331@bennett.edu.in>
2024-08-07 21:07:04 +0530

    first commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home_social">
+      <a
+        href="https://www.instagram.com/yogesh_gurjar_harshana"
+        className="home_social_icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-instagram"></i>
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/yogeshharsana/"
+        className="home_social_icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="bx bxl-linkedin"></i>
+      </a>
+
+      <a
+        href="https://github.com/yogeshharsana"
+        className="home_social_icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
